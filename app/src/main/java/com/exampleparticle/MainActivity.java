bcb68fe5e84a266a17a8fae29f872002ecd9d748
zhangke <609975727@qq.com>
2016-12-24 19:32:56 +0800

    Initial commit

---
+++ app/src/main/java/com/exampleparticle/MainActivity.java
@@ -0,0 +1,45 @@
+package com.exampleparticle;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.exampleparticle.library.ParticleView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ParticleView particleAnimator = new ParticleView(MainActivity.this,3000);
+        particleAnimator.setOnAnimationListener(new ParticleView.OnAnimationListener() {
+            @Override
+            public void onAnimationStart(View view,Animator animation) {
+                view.setVisibility(View.INVISIBLE);
+            }
+            @Override
+            public void onAnimationEnd(View view,Animator animation) {
+
+            }
+        });
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                particleAnimator.boom(getWindow().getDecorView().findViewById(android.R.id.content));//
+            }
+        });
+
+
+
+
+
+
+    }
+}
